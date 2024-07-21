c3e798f8108878c32c7a6d73a3593760ab3dd818
PJ-Limo <pjslense@gmail.com>
2024-07-21 20:35:51 +0300

    updated fetch files

---
+++ fetch-data.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const fetchUserData = async (firstName, lastName) => {
+
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const dataContainer = document.getElementById('api-data');
+      
+        try {
+          const response = await fetch('https://jsonplaceholder.typicode.com/users');
+          then(res => {
+              return res.json();
+          })
+          .then( data => {
+              data.forEach( user => {
+                  const userList = '<li>${user.name}</li>';
+      
+                  document.querySelector('ul').insertAdjacentHTML('beforeend', userList)
+              })
+          })
+        } catch (error) {
+          console.log('Failed to load user data.', error);
+        }
+        
+        dataContainer.innerHTML = '';
+      
+    };
+});
